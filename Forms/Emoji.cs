879a997c46bc9b6f9ebf09138ba65d6bc6f7e4e5
TheIceDragonz <giuly03oros@gmail.com>
2025-01-19 21:01:41 +0100

    ClipboardHandler

--- Forms/Emoji.cs
+++ Forms/Emoji.cs
@@ -81,7 +81,14 @@ namespace VRCGalleryManager.Forms
 
                     if (openFileDialog.ShowDialog() == DialogResult.OK)
                     {
-                        UploadImage(openFileDialog.FileName);
+                        if (!emojiOpenTypePanel.Text.Contains("Type"))
+                        {
+                            UploadImage(openFileDialog.FileName);
+                        }
+                        else
+                        {
+                            DialogMessage.ShowMissingTypeDialog(this);
+                        }
                     }
                 }
             }
@@ -122,48 +129,7 @@ namespace VRCGalleryManager.Forms
 
         private void pasteButton_Click(object sender, EventArgs e)
         {
-            IDataObject data = Clipboard.GetDataObject();
-            if (data != null)
-            {
-                if (data.GetDataPresent(DataFormats.Bitmap))
-                {
-                    var image = (Image)data.GetData(DataFormats.Bitmap);
-
-                    string directoryPath = Path.Combine(Path.GetTempPath(), "VRCGalleryManager");
-                    Directory.CreateDirectory(directoryPath);
-                    string tempPath = Path.Combine(directoryPath, $"Pasted-Image_{Guid.NewGuid()}.png");
-                    image.Save(tempPath, System.Drawing.Imaging.ImageFormat.Png);
-                    UploadImage(tempPath);
-                    NotificationManager.ShowNotification("Image pasted and saved successfully!", "Paste Image", NotificationType.Success);
-                }
-                else if (data.GetDataPresent(DataFormats.FileDrop))
-                {
-                    string[] files = (string[])data.GetData(DataFormats.FileDrop);
-                    if (files.Length > 0)
-                    {
-                        if (!emojiOpenTypePanel.Text.Contains("Type"))
-                        {
-                            pasteButton.Enabled = false;
-
-                            UploadImage(files[0]);
-
-                            pasteButton.Enabled = true;
-                        }
-                        else
-                        {
-                            DialogMessage.ShowMissingTypeDialog(this);
-                        }
-                    }
-                }
-                else
-                {
-                    NotificationManager.ShowNotification("No image or file found in the clipboard!", "Error", NotificationType.Error);
-                }
-            }
-            else
-            {
-                NotificationManager.ShowNotification("Clipboard is empty!", "Error", NotificationType.Error);
-            }
+            ClipboardHandler.ClipboardDataImage(pasteButton, UploadImage);
         }
     }
 }
--- Forms/Prints.cs
+++ Forms/Prints.cs
@@ -93,41 +93,7 @@ namespace VRCGalleryManager.Forms
         }
         private void pasteButton_Click(object sender, EventArgs e)
         {
-            IDataObject data = Clipboard.GetDataObject();
-            if (data != null)
-            {
-                if (data.GetDataPresent(DataFormats.Bitmap))
-                {
-                    var image = (Image)data.GetData(DataFormats.Bitmap);
-
-                    string directoryPath = Path.Combine(Path.GetTempPath(), "VRCGalleryManager");
-                    Directory.CreateDirectory(directoryPath);
-                    string tempPath = Path.Combine(directoryPath, $"Pasted-Image_{Guid.NewGuid()}.png");
-                    image.Save(tempPath, System.Drawing.Imaging.ImageFormat.Png);
-                    UploadImage(tempPath);
-                    NotificationManager.ShowNotification("Image pasted and saved successfully!", "Paste Image", NotificationType.Success);
-                }
-                else if (data.GetDataPresent(DataFormats.FileDrop))
-                {
-                    string[] files = (string[])data.GetData(DataFormats.FileDrop);
-                    if (files.Length > 0)
-                    {
-                        pasteButton.Enabled = false;
-
-                        UploadImage(files[0]);
-
-                        pasteButton.Enabled = true;
-                    }
-                }
-                else
-                {
-                    NotificationManager.ShowNotification("No image or file found in the clipboard!", "Error", NotificationType.Error);
-                }
-            }
-            else
-            {
-                NotificationManager.ShowNotification("Clipboard is empty!", "Error", NotificationType.Error);
-            }
+            ClipboardHandler.ClipboardDataImage(pasteButton, UploadImage);
         }
     }
 }
