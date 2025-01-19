b832970edf0f45a573339e7c715d653f4d38a22e
TheIceDragonz <giuly03oros@gmail.com>
2025-01-19 18:19:29 +0100

    Multiple Notification

--- Core/NotificationManager.cs
+++ Core/NotificationManager.cs
@@ -5,19 +5,21 @@ namespace VRCGalleryManager.Core
 {
     public static class NotificationManager
     {
+        private static readonly List<RoundedPanel> ActiveNotifications = new List<RoundedPanel>();
+        private const int PanelWidth = 310;
+        private const int PanelHeight = 80;
+        private const int Spacing = 10;
+
         public static void ShowNotification(string message, string title, NotificationType type)
         {
             Form mainForm = Application.OpenForms["MainPanel"];
 
-            int panelWidth = 310;
-            int panelHeight = 80;
-
             RoundedPanel notificationPanel = new RoundedPanel
             {
                 BorderRadius = 10,
-                Size = new Size(panelWidth, panelHeight),
+                Size = new Size(PanelWidth, PanelHeight),
                 BackColor = type == NotificationType.Success ? Color.LightGreen : Color.LightCoral,
-                Location = new Point(-panelWidth, mainForm.ClientSize.Height - 90),
+                Location = new Point(-PanelWidth, mainForm.ClientSize.Height - (PanelHeight + Spacing)),
                 Anchor = AnchorStyles.Bottom | AnchorStyles.Left
             };
 
@@ -37,13 +39,19 @@ namespace VRCGalleryManager.Core
                 Font = new Font("Arial", 10),
                 ForeColor = Color.Black,
                 AutoSize = true,
-                MaximumSize = new Size(panelWidth - 20, 0),
+                MaximumSize = new Size(PanelWidth - 20, 0),
                 Location = new Point(10, 40)
             };
             notificationPanel.Controls.Add(messageLabel);
 
             mainForm.Controls.Add(notificationPanel);
             notificationPanel.BringToFront();
+
+            lock (ActiveNotifications)
+            {
+                ActiveNotifications.Add(notificationPanel);
+                AdjustNotificationPositions();
+            }
 
             Timer slideInTimer = new Timer { Interval = 5 };
             slideInTimer.Tick += (s, e) =>
@@ -59,21 +67,44 @@ namespace VRCGalleryManager.Core
             closeTimer.Tick += (s, e) =>
             {
                 closeTimer.Stop();
-                Timer slideOutTimer = new Timer { Interval = 5 };
-                slideOutTimer.Tick += (s2, e2) =>
-                {
-                    if (notificationPanel.Location.X > -panelWidth)
-                        notificationPanel.Location = new Point(notificationPanel.Location.X - 20, notificationPanel.Location.Y);
-                    else
-                    {
-                        slideOutTimer.Stop();
-                        mainForm.Controls.Remove(notificationPanel);
-                        notificationPanel.Dispose();
-                    }
-                };
-                slideOutTimer.Start();
+                RemoveNotification(notificationPanel);
             };
             closeTimer.Start();
+        }
+
+        private static void RemoveNotification(RoundedPanel notificationPanel)
+        {
+            Timer slideOutTimer = new Timer { Interval = 5 };
+            slideOutTimer.Tick += (s, e) =>
+            {
+                if (notificationPanel.Location.X > -PanelWidth)
+                    notificationPanel.Location = new Point(notificationPanel.Location.X - 20, notificationPanel.Location.Y);
+                else
+                {
+                    slideOutTimer.Stop();
+                    lock (ActiveNotifications)
+                    {
+                        ActiveNotifications.Remove(notificationPanel);
+                        notificationPanel.Parent.Controls.Remove(notificationPanel);
+                        notificationPanel.Dispose();
+                        AdjustNotificationPositions();
+                    }
+                }
+            };
+            slideOutTimer.Start();
+        }
+
+        private static void AdjustNotificationPositions()
+        {
+            lock (ActiveNotifications)
+            {
+                int currentY = Application.OpenForms["MainPanel"].ClientSize.Height - (PanelHeight + Spacing);
+                foreach (var panel in ActiveNotifications)
+                {
+                    panel.Location = new Point(panel.Location.X, currentY);
+                    currentY -= (PanelHeight + Spacing);
+                }
+            }
         }
     }
 
