c1b5d29e10c1c4b70954539f0392f0f6542d75da
TheIceDragonz <giuly03oros@gmail.com>
2025-02-18 21:29:09 +0100

    Fix ImageResizer

--- Core/Helpers/ImageResizer.cs
+++ Core/Helpers/ImageResizer.cs
@@ -8,25 +8,23 @@ namespace VRCGalleryManager.Core.Helpers
         {
             using var input = Image.FromFile(imagePath);
             int target = 1024;
-            if (input.Width <= target && input.Height <= target)
+            if (input.Width == input.Height)
             {
                 return imagePath;
             }
-            float ratio = (float)input.Width / input.Height;
-            int newW = input.Width > input.Height ? target : (int)(target * ratio);
-            int newH = input.Width > input.Height ? (int)(target / ratio) : target;
+            int newSize = Math.Max(input.Width, input.Height);
             var tempDir = Path.Combine(Path.GetTempPath(), "VRCGalleryManager");
             Directory.CreateDirectory(tempDir);
             var outPath = Path.Combine(tempDir, $"resized_{Guid.NewGuid()}.png");
-            using var bmp = new Bitmap(newW, newH, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            using var bmp = new Bitmap(newSize, newSize, PixelFormat.Format32bppArgb);
             using (var g = Graphics.FromImage(bmp))
             {
-                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-                g.DrawImage(input, 0, 0, newW, newH);
+                g.Clear(Color.Transparent);
+                int offsetX = (newSize - input.Width) / 2;
+                int offsetY = (newSize - input.Height) / 2;
+                g.DrawImage(input, offsetX, offsetY, input.Width, input.Height);
             }
-            bmp.Save(outPath, System.Drawing.Imaging.ImageFormat.Png);
+            bmp.Save(outPath, ImageFormat.Png);
             return outPath;
         }
 
@@ -38,11 +36,11 @@ namespace VRCGalleryManager.Core.Helpers
             if (input.Width <= 2024 && input.Height <= 2024)
             {
                 var outPath = Path.Combine(tempDir, $"converted_{Guid.NewGuid()}.png");
-                using var bmp = new Bitmap(input.Width, input.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                using var bmp = new Bitmap(input.Width, input.Height, PixelFormat.Format32bppArgb);
                 using var g = Graphics.FromImage(bmp);
                 g.Clear(Color.Transparent);
                 g.DrawImage(input, new Rectangle(0, 0, input.Width, input.Height));
-                bmp.Save(outPath, System.Drawing.Imaging.ImageFormat.Png);
+                bmp.Save(outPath, ImageFormat.Png);
                 return outPath;
             }
             int canvasW = 2048, canvasH = (int)(canvasW / 16.0 * 9);
@@ -50,14 +48,11 @@ namespace VRCGalleryManager.Core.Helpers
             int newW = (int)(input.Width * scale), newH = (int)(input.Height * scale);
             int offsetX = (canvasW - newW) / 2, offsetY = (canvasH - newH) / 2;
             var outPathResized = Path.Combine(tempDir, $"resized_{Guid.NewGuid()}.png");
-            using var bmpResized = new Bitmap(canvasW, canvasH, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            using var bmpResized = new Bitmap(canvasW, canvasH, PixelFormat.Format32bppArgb);
             using var g2 = Graphics.FromImage(bmpResized);
             g2.Clear(Color.Transparent);
-            g2.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-            g2.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-            g2.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
             g2.DrawImage(input, new Rectangle(offsetX, offsetY, newW, newH));
-            bmpResized.Save(outPathResized, System.Drawing.Imaging.ImageFormat.Png);
+            bmpResized.Save(outPathResized, ImageFormat.Png);
             return outPathResized;
         }
     }
